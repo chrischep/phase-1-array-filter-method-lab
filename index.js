950a598fb0fa3ff39e134072d067678c518d0743
chrischep <christine.chepleting@student.moringaschool.com>
2022-08-23 12:55:21 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+ // Code your solution here
+ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+ function findMatching(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase() === name.toLowerCase()
+        // console.log(drivers);
+    });
+  };
+  findMatching(drivers);
+
+  function fuzzyMatch(drivers, middleName) {
+    return drivers.filter(function(driver){
+        return driver.slice(0, middleName.length).toLowerCase() === middleName.toLowerCase()
+    });
+  };
+
+  function matchName(drivers, property) {
+    return drivers.filter(function(driver){
+        return driver.name.toLowerCase() === property.toLowerCase()
+    });
+  };
+ 
